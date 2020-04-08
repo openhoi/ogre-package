058d17ce3d303ea0d3407a1638ea8785e1854e7c
Thomas Reiser <reiser.thomas@gmail.com>
2020-04-08 15:45:13 +0200

    Fix windows build

--- package.bat
+++ package.bat
@@ -157,9 +157,6 @@ if not "%BUILD_ONLY%" == "y" (
 
 echo %LINEBEG% Compiling OGRE...
 set OGRE_VERSION=%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_PATCH%
-if /I "%CI%" == "true" (
-  setx OGRE_VERSION "%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_PATCH%"
-)
 set OGRE_TAG=v%OGRE_VERSION%
 
 git -C ogre reset --hard %OGRE_TAG%
